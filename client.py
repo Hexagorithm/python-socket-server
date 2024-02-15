2a613745c2d465ffd6093fc3ae7e71ce4a4b23b8
Hexagoritm <139809645+Hexagorithm@users.noreply.github.com>
2024-02-15 22:58:12 +0100

    file-dump

---
+++ client.py
@@ -0,0 +1,100 @@
+#!/usr/bin/python3
+import socket
+import sys
+
+
+OPTIONS = {'1','2','3','4'}
+PORT = 5050 
+SERVERIP = "192.168.33.6"
+SERVERADDRESS = (SERVERIP, PORT)
+MESSHEADER = 64 
+MESSFORMAT = "utf-8"
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+is_connected = False
+
+    
+
+
+def showOptions():
+    print("\n\n\n\n\n\n\n\n\n")
+    print("Options for the connection:")
+    print("  1 - connect to server")
+    print("  2 - send message to server")
+    print("  3 - disconnect from server")
+    print("  4 - close program")
+
+def getOption():
+    global option
+    option = None
+    while option not in OPTIONS:
+        option = str(input("Input option: "))
+        if option not in OPTIONS:
+            print("Option invalid!")
+
+def handleOptions(option):
+    if option == '1':
+        print("Connecting...",end="")
+        connect()
+        print("done!")
+
+    elif option == '2':
+        message = str(input("Message:"))
+        send(message)
+
+    elif option == '3':
+        closeConnection()
+
+    elif option == '4':
+        if is_connected:
+            closeConnection()
+        sys.exit()
+
+    else:
+        print("What the fuck?")
+        exit()
+
+def send(message):
+    if not is_connected:
+        print("E:Can\'t send message: user not connected.")
+        return None
+    message_encoded = message.encode(MESSFORMAT)
+    message_length = len(message)
+    message_length_encoded = str(message_length).encode(MESSFORMAT) #we will be sending the lenght as a message too
+    message_length_encoded_padded = message_length_encoded + b' '* (MESSHEADER - len(message_length_encoded))#make sure the length is the appropriate header length
+    client_socket.send(message_length_encoded_padded)
+    client_socket.send(message_encoded)
+    return None
+
+def connect():
+    global client_socket,is_connected
+    client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    client_socket.connect(SERVERADDRESS) # as opposed to server_socket.bind()
+    is_connected = True
+
+
+def closeConnection():
+    global is_connected
+    if not is_connected:
+        print("E:Can\'t close connection: user not connected")
+        return None
+    send(DISCONNECT_MESSAGE)
+
+    client_socket.close()
+    is_connected = False
+    print("Connection closed!")
+
+def main():
+    try:
+        while True:
+            showOptions()
+            getOption()
+            handleOptions(option)
+    except KeyboardInterrupt:
+        if is_connected:
+            closeConnection()
+        print("Program closed manually.") 
+        sys.exit()
+
+if __name__ == "__main__":
+    main()
---
+++ server.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+
+import sys
+import socket
+import threading
+
+PORT = 5050 #server runs here
+SERVERIP = socket.gethostbyname(socket.gethostname())
+ADDRESS = (SERVERIP, PORT)
+MESSHEADER = 64 #fixed byte length of message header
+MESSFORMAT = "utf-8"
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server_socket.bind(ADDRESS)
+
+def startServer():
+    print(f"[S]Listening on {SERVERIP}:{PORT} ...")
+    server_socket.listen() # enable connection
+    while True:
+        client_socket, client_addr = server_socket.accept()
+        thread = threading.Thread(target=clientHandling, args=(client_socket,client_addr),daemon=True)
+        thread.start()
+        print(f"[S]Online connections: {threading.active_count() - 1}")
+
+
+
+def clientHandling(client_socket,client_addr):
+    print(f"[+]{client_addr[0]}:{client_addr[1]}")
+    is_connected = True
+    
+    while is_connected:
+        message = receive(client_socket)
+        print(f"[M][{client_addr[0]}]:\'{message}\'") 
+         
+        if message == DISCONNECT_MESSAGE:
+            print(f"[-][{client_addr[0]}]")
+            is_connected = False
+        elif message == None:
+            print(f"[client_addr[0]] connection message registered!")
+    client_socket.close()
+   
+    return None
+
+def receive(client_socket):
+    message_length = client_socket.recv(MESSHEADER).decode(MESSFORMAT) #length of message
+    if message_length == "":
+        return None
+    message = client_socket.recv(int(message_length)).decode(MESSFORMAT)
+    return message
+
+try:
+    startServer()
+except KeyboardInterrupt:
+    #implement looping through every client socket and close it
+    #when a thread is active, the server won't close propelly
+    print("Server closed manually!")
+    sys.exit()
